ab32aeca329f0558d8ac9276991a7deb1a9e3177
Edison Wang <ew040607@gmail.com>
2026-09-03 14:36:22 -0400

    feat: database connection

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"github.com/joho/godotenv"
+)
 
 // How long before a session starts we should have commands packed and ready.
 const PreSessionLeadTime = 5 * time.Minute
 
 // How often to re-check for a scheduled session when none is currently pending.
 const NoSessionPollInterval = 60 * time.Second
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("no .env file found, using system env vars")
+	}
+}
 
 func main() {
 	for {
